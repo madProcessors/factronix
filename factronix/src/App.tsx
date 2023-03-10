9a39e76eaaa28a903a9d101ae644baa74938c515
Bryan Calisto <bryancalisto@yahoo.es>
2023-03-09 23:11:13 -0500

    Installed mui, but having issues with button onclick

--- factronix/src/App.tsx
+++ factronix/src/App.tsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import { invoke } from "@tauri-apps/api/tauri";
 import "./App.css";
+import { Button, Input } from "@mui/material";
 
 function App() {
   const [greetMsg, setGreetMsg] = useState("");
@@ -15,36 +16,9 @@ function App() {
   return (
     <div className="container">
       <h1>Welcome to Factronix!</h1>
-
-      <div className="row">
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo vite" alt="Vite logo" />
-        </a>
-        <a href="https://tauri.app" target="_blank">
-          <img src="/tauri.svg" className="logo tauri" alt="Tauri logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-
-      <p>Click on the Tauri, Vite, and React logos to learn more.</p>
-
-      <div className="row">
-        <form
-          onSubmit={(e) => {
-            e.preventDefault();
-            greet();
-          }}
-        >
-          <input
-            id="greet-input"
-            onChange={(e) => setName(e.currentTarget.value)}
-            placeholder="Enter a name..."
-          />
-          <button type="submit">Greet</button>
-        </form>
-      </div>
+      <Input value={name} onChange={(e) => setName(e.target.value)} />
+      <Button onClick={greet}>a button</Button>
+      <p>{name}</p>
       <p>{greetMsg}</p>
     </div>
   );
