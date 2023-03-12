a908ef0d3d7ee35f6ca61a234ee34230af8de935
Bryan Calisto <bryancalisto@yahoo.es>
2023-03-12 18:52:46 -0500

    Feature/setting eslint up (#1)
    * Configured eslint and ran it
    
    * Finished configuring first eslint implementation
    
    * Removed comment

--- factronix/src/App.tsx
+++ factronix/src/App.tsx
@@ -1,26 +1,26 @@
-import { useState } from "react";
-import { invoke } from "@tauri-apps/api/tauri";
-import "./App.css";
-import { Button, Container, Input } from "@mui/material";
+import { useState } from 'react'
+import { invoke } from '@tauri-apps/api/tauri'
+import './App.css'
+import { Button, Container, Input } from '@mui/material'
 
-function App() {
-  const [greetMsg, setGreetMsg] = useState("");
-  const [name, setName] = useState("");
+const App = () => {
+  const [greetMsg, setGreetMsg] = useState('')
+  const [name, setName] = useState('')
 
-  async function greet() {
+  const greet = async () => {
     // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
-    setGreetMsg(await invoke("greet", { name }));
+    setGreetMsg(await invoke('greet', { name }))
   }
 
   return (
     <Container>
       <h1>Welcome to Factronix!</h1>
-      <Input value={name} onChange={(e) => setName(e.target.value)} />
+      <Input value={name} onChange={(e) => { setName(e.target.value) }} />
       <Button onClick={greet}>Greet</Button>
       <p>{name}</p>
       <p>{greetMsg}</p>
     </Container>
-  );
+  )
 }
 
-export default App;
+export default App
